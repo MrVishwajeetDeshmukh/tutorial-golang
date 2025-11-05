9950fdc0ff92b39d7fcf286abe53ea630002d494
Vishwajeet Deshmukh <69856637+MrVishwajeetDeshmukh@users.noreply.github.com>
2025-11-05 23:29:25 +0530

    added comment

---
+++ ch03/comment/comment.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	// This is a comment.
+	// Comments are usually written to explain code.
+	fmt.Println("Hello!")
+
+	// The following line is a comment, so it won’t be executed.
+	// fmt.Println("This code will not run.")
+
+	/*
+	   When you want to write a multi-line comment, use this style.
+	   All text inside this commented block is ignored by the compiler.
+
+	   fmt.Println("This code also won't run.")
+	*/
+}
